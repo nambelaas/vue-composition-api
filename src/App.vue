72400d6392f1e3040e91f6f2e9e7605432f40d91
nambelaas <salmaniseif@gmail.com>
2026-05-10 17:13:25 +0700

    add data reactive using reactive function

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script lang="ts">
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
+
 export default {
   setup() {
     const message = ref('Hello')
-    const quantity = ref(0)
+    const quantity = ref(0) // dipake untuk struktur data yang sederhana
+    const item = reactive({
+      name: 'Product 1',
+      price: 100,
+    }) // dipake untuk struktur data yang kompleks, seperti object atau array
 
     const increment = () => quantity.value++
     const decrement = () => {
@@ -14,18 +19,26 @@ export default {
       return quantity.value
     }
 
+    const swapProduct = () => {
+      item.name = 'Product 2'
+      item.price = 150
+    }
+
     return {
       message,
       quantity,
       increment,
       decrement,
+      item,
+      swapProduct,
     }
   },
 }
 </script>
 
 <template>
-  <h1>{{ message }}</h1>
+  <h1>{{ item.name }} : {{ item.price }}</h1>
+  <button @click="swapProduct">Swap Product</button>
   <h2>{{ quantity }}</h2>
   <button @click="increment">+</button>
   <button @click="decrement">-</button>
