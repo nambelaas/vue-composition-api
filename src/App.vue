f530c6c8381057822d35dac2691af31c6b2836d7
nambelaas <salmaniseif@gmail.com>
2026-05-10 17:07:29 +0700

    add reactive data using ref function

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <script lang="ts">
+import { ref } from 'vue'
 export default {
   setup() {
-    const message = 'Hello'
+    const message = ref('Hello')
+    const quantity = ref(0)
+
+    const increment = () => quantity.value++
+    const decrement = () => {
+      if (quantity.value > 0) {
+        return quantity.value--
+      }
+
+      return quantity.value
+    }
 
     return {
       message,
+      quantity,
+      increment,
+      decrement,
     }
   },
 }
@@ -12,4 +26,7 @@ export default {
 
 <template>
   <h1>{{ message }}</h1>
+  <h2>{{ quantity }}</h2>
+  <button @click="increment">+</button>
+  <button @click="decrement">-</button>
 </template>
