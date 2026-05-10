f923aadb55487cf71814c78221c8ca6c67f20d52
nambelaas <salmaniseif@gmail.com>
2026-05-10 16:59:44 +0700

    add composition api setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+export default {
+  setup() {
+    const message = 'Hello'
+
+    return {
+      message,
+    }
+  },
+}
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
